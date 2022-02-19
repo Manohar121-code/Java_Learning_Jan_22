b7e7ea092a69db37d1fa4d37af2058251adbe4a2
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2022-02-19 09:56:46 +0530

    Type_Casting_03

--- src/javase/learning/oops/typeCasting/ClassCastingEx.java
+++ src/javase/learning/oops/typeCasting/ClassCastingEx.java
@@ -15,7 +15,7 @@ public class ClassCastingEx {
 
 //		Child obj2 = new Parent();
 //		obj2.m2();
-
+		
 		Parent obj3 = new Child();// upcasting
 
 		Child c2 = (Child) obj3;// downcasting
@@ -27,5 +27,21 @@ public class ClassCastingEx {
 			System.out.println("obj4 is instance of Child");
 			Child c3 = (Child) obj4;//downcasting
 		}
+		
+		int i = 10;
+		
+		Parent obj5 = new Child();//upcasting
+		
+		Child c5 = (Child) obj5;//downcasting
+		
+		System.out.println("-------------------------------------");
+		
+		Parent obj6 = new SubChild();// upcasting
+		
+		SubChild sc6 = (SubChild) obj6;// downcasting
+		
+		Parent obj7 = new SubChild();//upcasting
+		Child c7 = (Child) obj7;//downcasting
+		SubChild sc7 = (SubChild) c7;//downcasting
 	}
 }
---
+++ src/javase/learning/oops/typeCasting/SubChild.java
@@ -0,0 +1,9 @@
+package javase.learning.oops.typeCasting;
+
+public class SubChild extends Child {
+
+	public void m3() {
+		System.out.println("Subchild m3() called");
+	}
+	
+}
