ff565c6e9a22a5712ca2025178ee1a99998aaef0
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2022-03-09 13:32:22 +0530

    ArrayList

--- src/javase/learning/part3/collection/list/ArrayListEx.java
+++ src/javase/learning/part3/collection/list/ArrayListEx.java
@@ -44,5 +44,64 @@ public class ArrayListEx {
 			Object object = (Object) itr.next();
 			System.out.println(object);
 		}
+		
+		System.out.println("----------------");
+		
+		list.add(1, "Java");
+		printList(list);
+		
+		List list2 = new ArrayList();
+		list2.add(10);
+		list2.add(false);
+		list2.add(87.56);
+		
+		list.addAll(list2);
+		printList(list);
+		
+		//clear all elements from collection
+//		list.clear();
+//		System.out.println(list.size());
+		
+		boolean contains = list.contains("Python");
+		System.out.println(contains);
+		
+		int indexOf = list.indexOf('T');
+		System.out.println(indexOf);
+		
+		int lastIndexOf = list.lastIndexOf(10);
+		System.out.println(lastIndexOf);
+		
+//		list.clear();
+		boolean empty = list.isEmpty();
+		System.out.println(empty);
+		
+		System.out.println("---------------");
+
+		list.add(7, 3);
+		printList(list);
+		
+		Object remove = list.remove(2);
+		printList(list);
+		
+		list.remove(87.56);
+		printList(list);
+		
+		Integer iObj = new Integer(3);
+		list.remove(iObj);
+		printList(list);
+		
+		list.set(4, "golang");
+		printList(list);
+		
+		List subList = list.subList(2, 5);
+		printList(subList);
+	}
+	
+	private static void printList(List list) {
+		for (Object o : list) {
+			System.out.println(o);
+		}
+		
+		System.out.println("----------------");
 	}
 }
